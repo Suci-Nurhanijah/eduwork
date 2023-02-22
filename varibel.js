cfb22f804f0584a40cde85ec55c2d9cb7ce7192e
SuciNurjh <71775182+Suci-Nurhanijah@users.noreply.github.com>
2023-02-22 14:25:50 +0700

    Add files via upload

---
+++ varibel.js
@@ -0,0 +1,15 @@
+// const name = "Dia";
+
+// console.log(name);
+
+// const student = {
+//     name : "Satria",
+//     class : 2
+// }
+
+// student.faculty = "IT";
+// console.log(student)
+
+// const student = ['sandi', 'diah', 'ilham'];
+// student.push('sari');
+// console.log(student);
